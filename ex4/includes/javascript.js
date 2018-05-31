3c09b3754cd216be069453294ef90f032e6418ae
ShohamRoditi <shohamroditi@gmail.com>
2018-05-31 23:21:51 +0300

    ex4_final

--- ex4/includes/javascript.js
+++ ex4/includes/javascript.js
@@ -7,14 +7,13 @@ function checkAddress(){
 	var addToAddress = document.getElementById("address");
 	var addTo = document.getElementById("adrs");
     var addressError = document.createElement("h4");
-     // addToAddress.appendChild(addressError);
     var address=addToAddress.value;
 	for(i=0; i<4; i++){
 		if(address==arrAddress[i]){
 			 	 return true;	
 		}
 	}
-		 	//addToAddress.innerHTML = 'blablabla';
+		 	
 		 	addressError.innerHTML = "You are required to enter the one of the following four addresses: israel, usa, asia, india";
 		 	addTo.appendChild(addressError);
 			return false;
